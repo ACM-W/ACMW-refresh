11bd0228c7de5e789815edfe79d4fc8f50fa2870
Luke Higgott <higgottluke@gmail.com>
2017-09-25 00:03:07 -0700

    static assets, skeleton for homepage and about page

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.cli.command('sass-compile')
+def sass_compile():
+    """you need to have sass installed to run this"""
+    os.system('sass static/sass/gaia.scss static/css/gaia.css')
