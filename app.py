59adfb1762a89cc6dc1686c07205ca3e345fb112
Luke Higgott <higgottluke@gmail.com>
2017-10-09 00:46:39 -0700

    still going...

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import os
 
 app = Flask(__name__)
 
+
+# Routes
+# These match the URL to a function that tells Flask what HTML to send back in render_template
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -20,29 +23,33 @@ def members():
 def events_home():
     return render_template('events/index.html', bannerurl=url_for('static', filename='img/TITANS-scenic.jpg'), title='Events')
 
-@app.route('/events/<event>')
-def event_page(event):
-    if event is 'code-camp':
-        return render_template('events/code-camp.html', bannerurl=url_for('static', filename='img/events/code-camp.jpg'), title='Code Camp')
-    elif event is 'creative-coding':
-        return render_template('events/creative-coding.html', bannerurl=url_for('static', filename='img/events/creative-coding.jpg'), title='Creative Coding')
-    elif event is 'leadership-summit':
-        return render_template('events/leadership-summit.html', bannerurl=url_for('static', filename='img/events/leadership-summit.jpg'), title='Computer Science Diversity and Leadership Summit')
-    elif event is 'stem-expo':
-        return render_template('events/stem-expo.html', bannerurl=url_for('static', filename='img/events/stem-expo.jpg'), title='STEM Expo')
-    else:
-        return not_found('Page not found')
+@app.route('/events/code-camp')
+def code_camp():
+    return render_template('events/code-camp.html', bannerurl=url_for('static', filename='img/events/code-camp.jpg'), title='Code Camp')
 
+@app.route('/events/creative-coding')
+def creative_coding():
+    return render_template('events/creative-coding.html', bannerurl=url_for('static', filename='img/events/creative-coding.jpg'), title='Creative Coding')
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('error.html', bannerurl=url_for('static', filename='img/ACMWBackground.png', title='Page Not Found')), 404
+@app.route('/events/leadership-summit')
+def leadership_summit():
+    return render_template('events/leadership-summit.html', bannerurl=url_for('static', filename='img/events/leadership-summit.jpg'), title='Computer Science Diversity and Leadership Summit')
+
+@app.route('/events/stem-expo')
+def leadership_summit():
+    return render_template('events/stem-expo.html', bannerurl=url_for('static', filename='img/events/stem-expo.jpg'), title='STEM Expo')
 
 @app.route('/contact')
 def contact_us():
     return render_template('contact.html')
 
 
+# this is the code that runs when a request is sent with a URL that doesn't match any routes
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html', bannerurl=url_for('static', filename='img/ACMWBackground.png', title='Page Not Found')), 404
+
+# this is to define a custom command line command to compile the SASS files in static/sass into plain old css in static/css
 @app.cli.command('sass-compile')
 def sass_compile():
     """you need to have sass installed to run this"""
